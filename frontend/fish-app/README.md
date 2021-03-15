9111c2505cd969e6b7f7228d54d7bc4a2173644a
powerticket <jwp0530@gmail.com>
2021-03-15 23:39:54 +0900

    [Feat] Add bottom navigation bar

---
+++ frontend/fish-app/README.md
@@ -0,0 +1,6 @@
+### 210315
+
+- Bottom Navigation Bar 생성
+
+  ![image-20210315233749245](README.assets/image-20210315233749245.png)
+
--- frontend/fish-app/types.tsx
+++ frontend/fish-app/types.tsx
@@ -4,14 +4,24 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  FishBook: undefined;
+  Ranking: undefined;
+  Profile: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type HomeParamList = {
+  HomeScreen: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+export type FishBookParamList = {
+  FishBookScreen: undefined;
+};
+
+export type RankingParamList = {
+  RankingScreen: undefined;
+};
+
+export type ProfileParamList = {
+  ProfileScreen: undefined;
 };
