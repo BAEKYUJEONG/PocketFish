61b64d0bca2f408b7c30e0f0c5a34df0254bce6f
Jamie Lee <itzjamie96@gmail.com>
2021-03-16 14:35:35 +0900

    initial commit (backend)

---
+++ backend/fish-server/src/main/java/com/a202/fishserver/FishServerApplication.java
@@ -0,0 +1,13 @@
+package com.a202.fishserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishServerApplication.class, args);
+	}
+
+}
