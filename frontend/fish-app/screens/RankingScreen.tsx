4648d89de7a9e814f45c53c374cb973a8e074afd
powerticket <jwp0530@gmail.com>
2021-03-22 21:47:35 +0900

    [Feat] Add ranking screen

--- frontend/fish-app/screens/RankingScreen.tsx
+++ frontend/fish-app/screens/RankingScreen.tsx
@@ -1,17 +1,48 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
-export default function RankingScreen() {
+import RankerBigView from "../components/ranking/RankerBigView";
+import RankerSmallView from "../components/ranking/RankerSmallView";
+
+export default function Home() {
+  const [rankerView, setRankerView] = React.useState(0);
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Ranking</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <View style={styles.headerView}>
+        <Text style={styles.instructions}>
+          포켓피쉬 영광의 Top 3를 만나보세요!
+        </Text>
+      </View>
+      <View style={styles.contentView}>
+        {!rankerView ? (
+          <>
+            <RankerBigView rank="1" />
+            <RankerBigView rank="2" />
+            <RankerBigView rank="3" />
+          </>
+        ) : (
+          arr.map((value) => (
+            <RankerSmallView key={value} rank={String(value)} />
+          ))
+        )}
+      </View>
+      <View style={styles.footerView}>
+        <TouchableOpacity
+          onPress={() => {
+            if (!rankerView) {
+              setRankerView(1);
+            } else {
+              setRankerView(0);
+            }
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Top 50 보기</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -19,6 +50,19 @@ export default function RankingScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerView: {
+    marginTop: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerView: {
+    marginBottom: 5,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -26,9 +70,28 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
+  loginView: {
+    flex: 1,
     marginVertical: 30,
     height: 1,
     width: "80%",
+    alignItems: "flex-end",
+  },
+  instructions: {
+    color: "#0476D9",
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#0560CF",
+    width: "90%",
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#AED6E8",
   },
 });
