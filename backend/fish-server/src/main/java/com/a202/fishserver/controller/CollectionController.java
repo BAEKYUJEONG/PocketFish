997e5ee44c92aa5449fa8880723c7484563c6eb5
kimmk0924 <kimmk0924@gmail.com>
2021-03-25 15:48:11 +0900

    [refactor] 내 보관함 조회 시 body -> param 으로 변경

--- backend/fish-server/src/main/java/com/a202/fishserver/controller/CollectionController.java
+++ backend/fish-server/src/main/java/com/a202/fishserver/controller/CollectionController.java
@@ -5,7 +5,7 @@ import com.a202.fishserver.dto.collection.CollectionGetDetailResponseDto;
 import com.a202.fishserver.dto.collection.CollectionGetRequestDto;
 import com.a202.fishserver.dto.collection.CollectionGetResponseDto;
 import com.a202.fishserver.dto.collection.CollectionPostRequestDto;
-import com.a202.fishserver.service.collection.CollectionService;
+import com.a202.fishserver.service.collection.CollectionServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
@@ -13,21 +13,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/collection")
 @AllArgsConstructor
 @Api(value = "물고기 도감 관리")
 public class CollectionController {
 
-    private final CollectionService collectionService;
+    private final CollectionServiceImpl collectionService;
 
     /**
      * 내 보관함 조회
      */
     @GetMapping
     @ApiOperation(value = "내 보관함 조회")
-    public Response getMyCollection(@RequestBody CollectionGetRequestDto dto) {
-        long userId = dto.getUser_id();
+    public Response getMyCollection(@RequestParam("user_id") int userId) {
         List<CollectionGetResponseDto> result = collectionService.getMyCollections(userId);
 
         return Response.builder()
@@ -78,6 +78,28 @@ public class CollectionController {
         return Response.builder()
                 .status(true)
                 .message("물고기 등록 성공")
+                .data(null)
+                .build();
+    }
+
+    /**
+     * 도감 정보 수정
+     */
+    @PutMapping("/{id}")
+    @ApiOperation(value = "도감 정보 수정")
+    public Response putCollection(@RequestBody CollectionPostRequestDto dto, @PathVariable("id") int collectionId) {
+        try {
+            collectionService.putCollection(dto, collectionId);
+        } catch (Exception e) {
+            return Response.builder()
+                    .status(false)
+                    .message(e.getMessage())
+                    .data(null)
+                    .build();
+        }
+        return Response.builder()
+                .status(true)
+                .message("도감 정보 수정 성공")
                 .data(null)
                 .build();
     }
