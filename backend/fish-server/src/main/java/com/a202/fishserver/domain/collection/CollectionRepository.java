e8f1a23d859d0fef6564e2529d07623752f11e35
Jamie Lee <itzjamie96@gmail.com>
2021-03-25 14:42:53 +0900

    [feat] RANKING system without redis

--- backend/fish-server/src/main/java/com/a202/fishserver/domain/collection/CollectionRepository.java
+++ backend/fish-server/src/main/java/com/a202/fishserver/domain/collection/CollectionRepository.java
@@ -1,5 +1,6 @@
 package com.a202.fishserver.domain.collection;
 
+import com.a202.fishserver.domain.fish.Fish;
 import com.a202.fishserver.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,6 @@ import java.util.List;
 
 public interface CollectionRepository extends JpaRepository<Collection, Integer> {
     List<Collection> findByUser(User user);
+
+    List<Collection> findByFish(Fish fish);
 }
