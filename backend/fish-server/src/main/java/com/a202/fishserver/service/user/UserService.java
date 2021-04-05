39785355e6ab4e838b5290dab3d27c71ece59bba
skeldtcan <skeldtcan@daum.net>
2021-04-05 10:40:02 +0900

    [feat] 사용자 정보 조회
    Former-commit-id: f6382d312ae8000c826180f9c5dfa0c39cdf1343

--- backend/fish-server/src/main/java/com/a202/fishserver/service/user/UserService.java
+++ backend/fish-server/src/main/java/com/a202/fishserver/service/user/UserService.java
@@ -2,9 +2,12 @@ package com.a202.fishserver.service.user;
 
 import com.a202.fishserver.dto.user.UserPostRequestDto;
 
+import java.util.HashMap;
+
 public interface UserService {
     void postUser(UserPostRequestDto dto) throws Exception;
     void putUserFlag(long userId, String accessToken) throws Exception;
     void putNickname(long userId, String nickname, String accessToken) throws Exception;
     long getUserIdByAccessToken(String acceessToken) throws Exception;
+    HashMap<String, Object> getUser(long userId) throws Exception;
 }
